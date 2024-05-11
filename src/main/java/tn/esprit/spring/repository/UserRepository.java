61fb14be83dc9e1828021d47d3b2b0a51bc5c06f
zairighayth <zairighayth@gmail.com>
2024-05-11 16:35:44 +0100

    second change

--- src/main/java/tn/esprit/spring/repository/UserRepository.java
+++ src/main/java/tn/esprit/spring/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import tn.esprit.spring.entities.User;
 //hello world from git
+//hello world fom user-management branch
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
